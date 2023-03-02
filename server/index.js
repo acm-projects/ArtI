3d3d3c842f2432da4783fd51a16d2921dad63438
Isaac Baldivas <ice.baldivas@gmail.com>
2023-03-01 21:44:41 -0600

    Started working on database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 
-const PORT = 3000 | process.env.PORT
+const PORT = 3000 || process.env.PORT
+
+mongoose.set('strictQuery', false)
+
+mongoose
+  .connect(
+    `mongodb+srv://admin:admin@arti.ck3bsyz.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log('Database is connected!')
+  })
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
