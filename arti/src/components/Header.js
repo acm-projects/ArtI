4abd6408b078b27cc49d2e3c5e767244a34c6576
shelllbelll <109366658+shelllbelll@users.noreply.github.com>
2023-03-02 23:06:11 -0600

    UX Login Page, NOT pretty

---
+++ arti/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+      <header>
+        <h1>Welcome</h1>
+        <h1>to</h1>
+        <h1>ArtI.</h1>
+      </header>
+    )
+  }
+  
+  export default Header
+  
