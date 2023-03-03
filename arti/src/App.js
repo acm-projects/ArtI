ffe9fed7ed9fec3586b6e8b916e225550afb0743
shelllbelll <109366658+shelllbelll@users.noreply.github.com>
2023-03-03 00:52:21 -0600

    Added router kinda(sign up to login and vice versa)

--- arti/src/App.js
+++ arti/src/App.js
@@ -1,18 +1,25 @@
 import Header from './components/Header.js'
 import HeaderTwo from './components/HeaderTwo.js'
 import LoginForm from './components/LoginForm.js'
+import Footer from './components/Footer.js'
+import SignUp from './components/SignUp.js'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
+    <Router>
       <div className = 'container'>
-        <Header />
-        <HeaderTwo />
-        <LoginForm />
+       <Header />
+       <HeaderTwo />
+       <LoginForm />
+       <Footer />
+       <SignUp /> 
       </div>
+      </Router>
   )
 }
 
-//either this or the one embedded in form,,waiting for reply
-//will do layout/formatting when CSS week hits (I think)
+//Routing Issue: things disappearing when putting route path
+//For now, SignUp is direct embed
 
 export default App
