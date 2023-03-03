20ca4fe1a7eb28ef681503211915fda3a025f542
shelllbelll <109366658+shelllbelll@users.noreply.github.com>
2023-03-03 01:12:54 -0600

    YAYYY.Routing work now

--- arti/src/App.js
+++ arti/src/App.js
@@ -3,23 +3,27 @@ import HeaderTwo from './components/HeaderTwo.js'
 import LoginForm from './components/LoginForm.js'
 import Footer from './components/Footer.js'
 import SignUp from './components/SignUp.js'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 const App = () => {
   return (
     <Router>
       <div className = 'container'>
-       <Header />
-       <HeaderTwo />
-       <LoginForm />
-       <Footer />
-       <SignUp /> 
+        <Routes>
+        <Route path='/' element={
+          <>
+           <Header />
+           <HeaderTwo />
+           <LoginForm />
+           <Footer />
+          </>
+        }
+        />
+       <Route path='/signup' element={<SignUp />} />
+       </Routes>
       </div>
       </Router>
   )
 }
-
-//Routing Issue: things disappearing when putting route path
-//For now, SignUp is direct embed
 
 export default App
