d13d06902ade8159a4f9bc77b61aed17f37c59d0
shelllbelll <109366658+shelllbelll@users.noreply.github.com>
2023-03-04 13:52:36 -0600

    form data prints in console!

--- arti/src/components/LoginForm.js
+++ arti/src/components/LoginForm.js
@@ -1,13 +1,55 @@
 import Button from './Button'
+import { useState } from 'react'
 
 const LoginForm = () => {
+
+  const [formData, setFormData] = useState({
+    username: "", password: ""
+  })
+
+  console.log(formData)
+
+  function handleChange(event) {
+    setFormData(prevFormData => {
+
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value
+
+      }
+    })
+    
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(formData)
+    console.log("submitted")
+  }
   return (
-    <form className = 'login-form' >
+    <form 
+    className = 'login-form' 
+    onSubmit={handleSubmit}
+    >
+
       <div className = 'form-control'>
-        <input type= 'text' placeholder= 'username' />
-      </div>
-      <div className = 'form-control'>
-        <input type= 'text' placeholder= 'password' />
+        <input 
+        type = 'text' 
+        placeholder = 'username'
+        onChange = {handleChange}
+        name = "username"
+        value = {formData.username}
+         />
+
+
+        <input 
+        type = 'text' 
+        placeholder = 'password'
+        onChange = {handleChange}
+        name = "password"
+        value = {formData.password}
+        />
+
       </div>
 
       <Button text = 'Log In.' />
