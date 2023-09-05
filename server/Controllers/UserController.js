9634af9b7eff8e8e2ee3ede76d2f44740ca2fb1a
Isaac Baldivas <ice.baldivas@gmail.com>
2023-09-05 01:06:11 -0500

    idk why it's not working

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -74,9 +74,18 @@ async function getUserAuthorized(req, res, next) {
     const username = req.body.username
 
     // verifying jwt token
-    const cert = fs
-      .readFileSync(path.join(__dirname, 'public', 'public.pem'))
-      .toString()
+    // const cert = fs
+    //   .readFileSync(path.join(__dirname, 'public', 'public.pem'), 'utf-8')
+    //   .toString()
+    const cert = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu1SU1LfVLPHCozMxH2Mo
+4lgOEePzNm0tRgeLezV6ffAt0gunVTLw7onLRnrq0/IzW7yWR7QkrmBL7jTKEn5u
++qKhbwKfBstIs+bMY2Zkp18gnTxKLxoS2tFczGkPLPgizskuemMghRniWaoLcyeh
+kd3qqGElvW/VDL5AaWTg0nLVkjRo9z+40RQzuVaE8AkAFmxZzow3x+VJYKdjykkJ
+0iT9wCS0DRTXu269V264Vf/3jvredZiKRkgwlL9xNAwxXFg0x/XFw005UWVRIkdg
+cKWTjpBP2dPwVZ4WWC+9aGVd+Gyn1o0CLelf4rEjGoXbAAEgAqeGUxrcIlbjXfbc
+mwIDAQAB
+-----END PUBLIC KEY-----`
     jwt.verify(clientToken, cert, async (err, decoded) => {
       if (err) {
         console.error('From getUserAuthorized: ', err.message)
